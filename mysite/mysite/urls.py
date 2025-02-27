fea98dbf9d4f70850a37dc34272d5b0923b29c1c
Kevin <kevinarroyolopez231@gmail.com>
2025-02-26 20:31:08 -0600

    primer commit

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('base/', views.base, name='base'),
+    path('acerca/', views.acerca, name='acerca'),
+    path('contacto/', views.contacto, name='contacto'),
+    path('costos/', views.costos, name='costos'),
+    path('funcionamiento/', views.funcionamiento, name='funcionamiento'),
+    path('nosotros/', views.nosotros, name='nosotros'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
---
+++ mysite/mysite/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'index.html')
+
+def base(request):
+    return render(request, "base.html", {})
+
+def acerca(request):
+    return render(request, "acercaProyecto.html", {})
+
+def contacto(request):
+    return render(request, "contacto.html", {})
+
+def costos(request):
+    return render(request, "costos.html", {})
+
+def funcionamiento(request):
+    return render(request, "funcionamiento.html", {})
+
+def nosotros(request):
+    return render(request, "sobreNosotros.html", {})
